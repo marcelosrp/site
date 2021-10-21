b225358c8f5dfea7e947923b8a560b41b45a2267
Marcelo Soares <soares.srp@gmail.com>
2021-10-21 16:33:21 -0300

    melhorando header

--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -7,12 +7,14 @@ export const Header = styled.header`
   display: flex;
   height: 6rem;
   justify-content: space-between;
+  padding: 0 1.5rem;
   position: sticky;
   top: 0;
   width: 100%;
   z-index: 2;
 
   @media (min-width: 700px) {
+    padding: 0 10rem;
     height: 10rem;
   }
 `
