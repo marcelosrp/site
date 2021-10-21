b078ee598bf6f4ea0305d3a211836fcc1be6d1be
Marcelo Soares <soares.srp@gmail.com>
2021-10-21 16:41:36 -0300

    adicionando efeito de loading na imagem dos projetos

--- src/components/CardProject/styles.js
+++ src/components/CardProject/styles.js
@@ -43,5 +43,25 @@ export const CardProject = styled.article`
 
   img {
     object-fit: cover;
+
+    background: #f6f7f8;
+    background-image: linear-gradient(
+      to right,
+      #f6f7f8 0%,
+      #edeef1 20%,
+      #f6f7f8 40%,
+      #f6f7f8 100%
+    );
+    background-size: 80rem 14rem;
+    animation: placeholderShimmer 1s linear infinite forwards;
+
+    @keyframes placeholderShimmer {
+      0% {
+        background-position: -40rem 0;
+      }
+      100% {
+        background-position: 40rem 0;
+      }
+    }
   }
 `
